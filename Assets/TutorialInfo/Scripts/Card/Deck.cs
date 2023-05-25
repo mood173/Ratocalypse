c57be78bbb09c5e24a1b337d71e5c5d9c438c0cf
qazxsw1240 <qazxsw010528@gmail.com>
2023-05-26 00:26:28 +0900

    덱 구현

---
+++ Assets/TutorialInfo/Scripts/Card/Deck.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeamOdd.Ratocalypse.Card
+{
+    public class Deck
+    {
+        private List<CardData> _cardList;
+
+        public Deck(params CardData[] cardDatas)
+        {
+            _cardList = cardDatas.ToList();
+        }
+
+        public void AddCard(CardData cardData)
+        {
+            _cardList.Add(cardData);
+        }
+
+        public void RemoveCards(ISet<int> indices)
+        {
+            SortedSet<int> sortedIndices = new SortedSet<int>(indices);
+            foreach (int i in sortedIndices.Reverse())
+            {
+                _cardList.RemoveAt(i);
+            }
+        }
+
+        public void RemoveCard(int index)
+        {
+            _cardList.RemoveAt(index);
+        }
+    }
+}
