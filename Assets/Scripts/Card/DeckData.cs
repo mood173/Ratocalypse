e4294529161fda2898601a35c157e6aca5e147bf
qazxsw1240 <qazxsw010528@gmail.com>
2023-06-27 07:44:19 +0900

    Deck -> DeckData 이름 변경

---
+++ Assets/Scripts/Card/DeckData.cs
@@ -0,0 +1,7 @@
+namespace TeamOdd.Ratocalypse.Card
+{
+    public class DeckData : CardDataCollection
+    {
+        public DeckData(params CardData[] cardDataItems) : base(cardDataItems) { }
+    }
+}
