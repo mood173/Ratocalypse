b52e0de168dd41fc3fdfbd802b2f012f45060c09
qazxsw1240 <qazxsw010528@gmail.com>
2023-06-27 07:43:59 +0900

    CardDataCollection 구현

---
+++ Assets/Scripts/Card/CardDataCollection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeamOdd.Ratocalypse.Card
+{
+    public class CardDataCollection : IEnumerable<CardData>
+    {
+        protected readonly List<CardData> _cardDataList;
+
+        public CardDataCollection(params CardData[] cardDataItems)
+        {
+            _cardDataList = cardDataItems.ToList();
+        }
+
+        public int Count { get => _cardDataList.Count; }
+
+        public CardData GetCard(int index)
+        {
+            return _cardDataList[index];
+        }
+
+        public void AddCard(CardData cardData)
+        {
+            _cardDataList.Add(cardData);
+        }
+
+        public void AddCards(params CardData[] cardDataItems)
+        {
+            foreach (CardData cardData in cardDataItems)
+            {
+                AddCard(cardData);
+            }
+        }
+        public void RemoveCard(int index)
+        {
+            _cardDataList.RemoveAt(index);
+        }
+
+        public void RemoveCards(ISet<int> indices)
+        {
+            SortedSet<int> sortedIndices = new SortedSet<int>(indices);
+            foreach (int i in sortedIndices.Reverse())
+            {
+                _cardDataList.RemoveAt(i);
+            }
+        }
+
+        public IEnumerator<CardData> GetEnumerator()
+        {
+            return _cardDataList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
