fc959700332a9d50e105c8adb6ef8f2cb9fe2c72
qazxsw1240 <qazxsw010528@gmail.com>
2023-07-03 19:30:10 +0900

    CardDataValue 추가 & CardData 수정

--- Assets/Scripts/Card/CardData.cs
+++ Assets/Scripts/Card/CardData.cs
@@ -2,11 +2,31 @@ namespace TeamOdd.Ratocalypse.Card
 {
     public class CardData
     {
-        public string CardDataId { get; }
+        public long CardDataId { get; }
+        public CardDataValue DataValue { get; }
 
-        public CardData(string cardDataId)
+        public CardData(long cardDataId)
         {
             CardDataId = cardDataId;
+            DataValue = new CardDataValue();
+        }
+
+        public CardData(long cardDataId, CardDataValue dataValue) : this(cardDataId)
+        {
+            DataValue = dataValue;
+        }
+    }
+
+    public class CardDataValue
+    {
+        public object ValueA { get; set; } = null;
+        public object ValueB { get; set; } = null;
+        public object ValueC { get; set; } = null;
+
+
+        public CardDataValue Clone()
+        {
+            return new CardDataValue();
         }
     }
 }
