bb1e9bcfffcd9d69433a08f1d8c9805fe89682ed
110V <sgb1616@gmail.com>
2023-07-05 00:36:58 +0900

    MeshCreator를 EditorWindow로 제작

--- Assets/Editor/MeshCreator/MeshCreator.cs
+++ Assets/Editor/MeshCreator/MeshCreator.cs
@@ -5,24 +5,9 @@ using UnityEngine;
 using Habrador_Computational_Geometry;
 using System.Linq;
 
-[RequireComponent(typeof(MeshFilter))]
-public class MeshCreator : MonoBehaviour
+public class MeshCreator
 {
-    [SerializeField]
-    private Sprite _sprite;
-    [SerializeField]
-    private float _thickness;
-
-    private MeshFilter _meshFilter;
-
-    private void Awake()
-    {
-        _meshFilter = GetComponent<MeshFilter>();
-
-        CreateMesh();
-    }
-
-    private void CreateMesh()
+    public Mesh CreateMesh(Sprite sprite, float thickness)
     {
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
@@ -30,10 +15,10 @@ public class MeshCreator : MonoBehaviour
         List<Vector2> points = new List<Vector2>();
 
 
-        float width = _sprite.bounds.size.x;
-        float height = _sprite.bounds.size.y;
+        float width = sprite.bounds.size.x;
+        float height = sprite.bounds.size.y;
 
-        _sprite.GetPhysicsShape(0, points);
+        sprite.GetPhysicsShape(0, points);
 
         for (int i = 0; i < points.Count; i++)
         {
@@ -47,7 +32,7 @@ public class MeshCreator : MonoBehaviour
             Vector2 point1 = points[i];
             Vector2 point2 = points[(i + 1) % points.Count];
 
-            AddWall(point1, point2, vertices, triangles, uvs, i == 0, i == points.Count - 1);
+            AddWall(point1, point2, thickness, vertices, triangles, uvs, i == 0, i == points.Count - 1);
         }
         List<MyVector2> hullVertices2d = points.Select(p => new MyVector2(p.x, p.y)).ToList();
         var triangulation = _EarClipping.Triangulate(hullVertices2d, optimizeTriangles: false);
@@ -57,24 +42,26 @@ public class MeshCreator : MonoBehaviour
             Vector2 point1 = triangle.p1.ToVector2();
             Vector2 point2 = triangle.p2.ToVector2();
             Vector2 point3 = triangle.p3.ToVector2();
-            AddTriangle(point1, point2, point3, vertices, triangles, uvs, true);
-            AddTriangle(point1, point2, point3, vertices, triangles, uvs, false);
+            AddTriangle(point1, point2, point3, thickness, vertices, triangles, uvs, true);
+            AddTriangle(point1, point2, point3, thickness, vertices, triangles, uvs, false);
         }
 
+        Mesh mesh = new Mesh();
 
-        _meshFilter.mesh.vertices = vertices.ToArray();
-        _meshFilter.mesh.triangles = triangles.ToArray();
-        _meshFilter.mesh.SetUVs(0, uvs);
-        _meshFilter.mesh.RecalculateNormals();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.SetUVs(0, uvs);
+        mesh.RecalculateNormals();
 
+        return mesh;
     }
 
-    private void AddTriangle(Vector3 a, Vector3 b, Vector3 c, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs, bool isFront)
+    private void AddTriangle(Vector3 a, Vector3 b, Vector3 c, float thickness, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs, bool isFront)
     {
         int frontBack = isFront ? 1 : -1;
-        a = a + new Vector3(0, 0, frontBack * _thickness / 2);
-        b = b + new Vector3(0, 0, frontBack * _thickness / 2);
-        c = c + new Vector3(0, 0, frontBack * _thickness / 2);
+        a = a + new Vector3(0, 0, frontBack * thickness / 2);
+        b = b + new Vector3(0, 0, frontBack * thickness / 2);
+        c = c + new Vector3(0, 0, frontBack * thickness / 2);
 
         Vector2 offset = !isFront ? Vector2.zero : new Vector2(0.5f, 0f);
 
@@ -118,7 +105,7 @@ public class MeshCreator : MonoBehaviour
         uvs.Add(new Vector2((x + 0.5f) / 2, y + 0.5f) + offset);
     }
 
-    private void AddWall(Vector2 point1, Vector2 point2, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs, bool first, bool last)
+    private void AddWall(Vector2 point1, Vector2 point2,float thickness, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs, bool first, bool last)
     {
         if (last)
         {
@@ -134,8 +121,8 @@ public class MeshCreator : MonoBehaviour
 
         if (first)
         {
-            Vector3 vertex0 = (Vector3)point1 + new Vector3(0, 0, _thickness / 2);
-            Vector3 vertex1 = (Vector3)point1 + new Vector3(0, 0, -_thickness / 2);
+            Vector3 vertex0 = (Vector3)point1 + new Vector3(0, 0, thickness / 2);
+            Vector3 vertex1 = (Vector3)point1 + new Vector3(0, 0, -thickness / 2);
             vertices.Add(vertex0);
             uvs.Add(Vector2.zero);
             vertices.Add(vertex1);
@@ -143,8 +130,8 @@ public class MeshCreator : MonoBehaviour
         }
 
 
-        Vector3 vertex2 = (Vector3)point2 + new Vector3(0, 0, _thickness / 2);
-        Vector3 vertex3 = (Vector3)point2 + new Vector3(0, 0, -_thickness / 2);
+        Vector3 vertex2 = (Vector3)point2 + new Vector3(0, 0, thickness / 2);
+        Vector3 vertex3 = (Vector3)point2 + new Vector3(0, 0, -thickness / 2);
 
 
         vertices.Add(vertex2);
@@ -161,6 +148,4 @@ public class MeshCreator : MonoBehaviour
         triangles.Add(currentIndex + 1);
         triangles.Add(currentIndex + 3);
     }
-
-
 }
