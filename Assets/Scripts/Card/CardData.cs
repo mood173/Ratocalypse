6c5b2107bbd5ab629239ccc8cca1051e92f4fd50
qazxsw1240 <qazxsw010528@gmail.com>
2023-07-25 17:58:48 +0900

    CardDataValue 변경

--- Assets/Scripts/Card/CardData.cs
+++ Assets/Scripts/Card/CardData.cs
@@ -24,14 +24,42 @@ namespace TeamOdd.Ratocalypse.Card
 
     public class CardDataValue
     {
-        public object ValueA { get; set; } = null;
-        public object ValueB { get; set; } = null;
-        public object ValueC { get; set; } = null;
+        public int Cost { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
 
+        public CardDataValue()
+        {
+            Cost = 0;
+            Title = "";
+            Description = "";
+        }
+
+        public CardDataValue(int cost, string title, string description)
+        {
+            Cost = cost;
+            Title = title;
+            Description = description;
+        }
+
+        public void SetCost(int cost)
+        {
+            Cost = cost;
+        }
+
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void SetDescription(string description)
+        {
+            Description = description;
+        }
 
         public virtual CardDataValue Clone()
         {
-            return new CardDataValue();
+            return new CardDataValue(Cost, Title, Description);
         }
     }
 }
